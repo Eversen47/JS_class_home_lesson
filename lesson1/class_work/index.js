9f4172d4f572ea49efc6c821735676eb8f18703a
Anton Lisin <kandzaki@gmail.com>
2022-02-22 14:04:32 +0300

    Добавил в readmi

---
+++ lesson1/class_work/index.js
@@ -0,0 +1,29 @@
+console.log("HELLO JAVASCRIPT");
+
+
+const planetName = "Земля";
+console.log("Земля"); 
+
+
+Data = new Date();
+console.log(Data); 
+document.write(Data);
+alert(Data);
+
+
+let articles = 21;
+articles = 18;
+console.log(articles);
+
+
+console.log("cash or cashless");
+
+
+let name = ['firstName', 'lastName', 'middleName'];
+console.log(name);
+
+
+
+
+
+
